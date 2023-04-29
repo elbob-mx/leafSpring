ffe61abca9e361e24a08e187aa8ec1fcddffb97c
Roberto Gutierrez <rob7th@gmail.com>
2023-04-29 15:34:38 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
       mobileHor: "640px",
       tablet: "767px",
       bigHor: "811px",
+      vertIpadMini: "819px",
       laptop: "976px",
       ipad: "1023px",
       hd: "1365px",
