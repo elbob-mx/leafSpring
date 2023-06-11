b916421a7429ac267337a5a1309404e7b7f6c4e0
Roberto Gutierrez <rob7th@gmail.com>
2023-06-11 14:19:55 -0600

    update text ipad mini formsubmit-com

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       mobileHor: "640px",
       tablet: "767px",
       bigHor: "811px",
-      vertIpadMini: "819px",
+      vertIpadMini: "820px",
       laptop: "976px",
       ipad: "1023px",
       hd: "1365px",
