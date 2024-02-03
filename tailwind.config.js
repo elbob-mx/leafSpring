79e83bdfab2134962a41797f0c4f1b7812e97aea
Roberto Gutierrez <rob7th@gmail.com>
2024-02-02 21:25:11 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html",
-  "./node_modules/flowbite/**/*.js"],
+  content: ["./*.html", "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       small: "0px",
@@ -21,20 +20,18 @@ module.exports = {
         robotoMono: "Roboto Mono, monospace",
         oswald: "Oswald, sans-serif",
         roboto: "Roboto, sans-serif",
-        prompt: "Prompt, sans-serif"
+        prompt: "Prompt, sans-serif",
       },
       colors: {
-        verdeLeaf: '#025541',
-        verdeClaroLeaf: '#029673',
-        grisLeaf: '#4C4C4C',
-        offWhite: '#F2F2F2',
-        offBlack: '#161616',
-        neonBlue: '#4AE7D9',
-        deepBlue: '#071e26'
+        verdeLeaf: "#025541",
+        verdeClaroLeaf: "#029673",
+        grisLeaf: "#4C4C4C",
+        offWhite: "#F2F2F2",
+        offBlack: "#161616",
+        neonBlue: "#4AE7D9",
+        deepBlue: "#071e26",
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("flowbite/plugin")],
 };
